76a4e607240c4c53d9c9175a4e63c0ee13533cb0
Your Name <you@example.com>
2023-05-05 00:28:43 +0330

    1_2 complete

--- 1_1(orm with localStorage)/dist/orm.js
+++ 1_1(orm with localStorage)/dist/orm.js
@@ -53,21 +53,41 @@ class UserService {
 }
 const userService = new UserService;
 //create Dom
-const firstName_create_input = document.querySelector('#firstName_create_input');
-const lastName_create_input = document.querySelector('#lastName_create_input');
-const email_create_input = document.querySelector('#email_create_input');
-const avatar_create_input = document.querySelector('#avatar_create_input');
+// const firstName_create_input:HTMLInputElement|null = document.querySelector<HTMLInputElement>('#firstName_create_input');
+// const lastName_create_input:HTMLInputElement|null = document.querySelector<HTMLInputElement>('#lastName_create_input');
+// const email_create_input:HTMLInputElement|null = document.querySelector<HTMLInputElement>('#email_create_input');
+// const avatar_create_input:HTMLInputElement|null = document.querySelector<HTMLInputElement>('#avatar_create_input');
+const firstName_create_input = document.getElementById("firstName_create_input");
+const lastName_create_input = document.getElementById("lastName_create_input");
+const email_create_input = document.getElementById("email_create_input");
+const avatar_create_input = document.getElementById("avatar_create_input");
 //update Dom
-const firstName_update_input = document.querySelector('#firstName_update_input');
-const lastName_update_input = document.querySelector('#lastName_update_input');
-const email_update_input = document.querySelector('#email_update_input');
-const avatar_update_input = document.querySelector('#avatar_update_input');
-const id_update_input = document.querySelector('#id_update_input');
+// const firstName_update_input:HTMLInputElement|null = document.querySelector<HTMLInputElement>('#firstName_update_input');
+// const lastName_update_input:HTMLInputElement|null = document.querySelector<HTMLInputElement>('#lastName_update_input');
+// const email_update_input:HTMLInputElement|null = document.querySelector<HTMLInputElement>('#email_update_input');
+// const avatar_update_input:HTMLInputElement|null = document.querySelector<HTMLInputElement>('#avatar_update_input');
+// const id_update_input:HTMLInputElement|null = document.querySelector<HTMLInputElement>('#id_update_input');
+const firstName_update_input = document.getElementById("firstName_update_input");
+const lastName_update_input = document.getElementById("lastName_update_input");
+const email_update_input = document.getElementById("email_update_input");
+const avatar_update_input = document.getElementById("avatar_update_input");
+const id_update_input = document.getElementById("id_update_input");
+//buttons dom
 const create_btn = document.getElementById("create_btn");
 const update_btn = document.getElementById("update_btn");
 const delete_btn = document.getElementById("delete_btn");
+const get_all_users_btn = document.getElementById("get_all_users_btn");
+const get_user_btn = document.getElementById("get_user_btn");
 //delete Dom
-const id_delete_input = document.querySelector('#id_delete_input');
+const id_delete_input = document.getElementById("id_delete_input");
+//text_area dom
+const text_area = document.getElementById("text_area");
+//get single user Dom
+const targetUser_id_input = document.getElementById("targetUser_id_input");
+const targetUser_firstName_input = document.getElementById("targetUser_firstName_input");
+const targetUser_lastName_input = document.getElementById("targetUser_lastName_input");
+const targetUser_email_input = document.getElementById("targetUser_email_input");
+const targetUser_avatar_input = document.getElementById("targetUser_avatar_input");
 //seed localstorage by reqres users
 const seed_btn = (_a = document.getElementById("seed")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", (e) => __awaiter(void 0, void 0, void 0, function* () {
     const responseObject = yield fetch("https://reqres.in/api/users");
@@ -84,34 +104,51 @@ create_btn === null || create_btn === void 0 ? void 0 : create_btn.addEventListe
     const id = Number(Date.now());
     const newUser = { first_name, last_name, email, avatar, id };
     userService.createUser(newUser);
-    if (firstName_create_input) {
-        firstName_create_input.value = '';
-    }
-    if (lastName_create_input) {
-        lastName_create_input.value = '';
-    }
-    if (email_create_input) {
-        email_create_input.value = '';
-    }
-    if (avatar_create_input) {
-        avatar_create_input.value = '';
-    }
+    alert("created successfully");
+    firstName_create_input.value = '';
+    lastName_create_input.value = '';
+    email_create_input.value = '';
+    avatar_create_input.value = '';
 });
 //update Button function
 update_btn === null || update_btn === void 0 ? void 0 : update_btn.addEventListener("click", (e) => {
-    const first_name = String(firstName_update_input === null || firstName_update_input === void 0 ? void 0 : firstName_update_input.value);
-    const last_name = String(lastName_update_input === null || lastName_update_input === void 0 ? void 0 : lastName_update_input.value);
-    const email = String(email_update_input === null || email_update_input === void 0 ? void 0 : email_update_input.value);
-    const avatar = String(avatar_update_input === null || avatar_update_input === void 0 ? void 0 : avatar_update_input.value);
-    const id = Number(id_update_input === null || id_update_input === void 0 ? void 0 : id_update_input.value);
+    const first_name = String(firstName_update_input.value);
+    const last_name = String(lastName_update_input.value);
+    const email = String(email_update_input.value);
+    const avatar = String(avatar_update_input.value);
+    const id = Number(id_update_input.value);
     const updateBody = { id, first_name, last_name, email, avatar };
     userService.updateUser(updateBody);
+    alert("updated successfully");
+    firstName_update_input.value = "";
+    lastName_update_input.value = "";
+    id_update_input.value = "";
+    email_update_input.value = "";
+    avatar_update_input.value = "";
 });
 //delete Button function
 delete_btn === null || delete_btn === void 0 ? void 0 : delete_btn.addEventListener("click", (e) => {
-    const id = Number(id_delete_input === null || id_delete_input === void 0 ? void 0 : id_delete_input.value);
+    const id = Number(id_delete_input.value);
     userService.deleteUser(id);
     if (!!id_delete_input) {
         id_delete_input.value = "";
+    }
+    alert("deleted successfully");
+    id_delete_input.value = "";
+});
+//get all users and print in index.html
+get_all_users_btn === null || get_all_users_btn === void 0 ? void 0 : get_all_users_btn.addEventListener("click", (e) => {
+    const usersList = userService.getAllUsers();
+    text_area.value = JSON.stringify(usersList);
+});
+//get single user
+get_user_btn === null || get_user_btn === void 0 ? void 0 : get_user_btn.addEventListener("click", (e) => {
+    const id = targetUser_id_input.value;
+    const targetUser = userService.readUser(Number(id));
+    if (targetUser) {
+        targetUser_firstName_input.value = targetUser.first_name;
+        targetUser_lastName_input.value = targetUser.last_name;
+        targetUser_email_input.value = targetUser.email;
+        targetUser_avatar_input.value = targetUser.avatar;
     }
 });
--- 1_1(orm with localStorage)/src/orm.ts
+++ 1_1(orm with localStorage)/src/orm.ts
@@ -39,7 +39,7 @@ interface User{
     this.saveUsers(users);
   }
 
-    private getAllUsers(): User[] {
+    public getAllUsers(): User[] {
     const data = localStorage.getItem(this.storageKey) || '[]';
     return JSON.parse(data);
     }
@@ -57,23 +57,44 @@ interface User{
 
 const userService = new UserService;
 //create Dom
-const firstName_create_input = document.querySelector<HTMLInputElement>('#firstName_create_input');
-const lastName_create_input = document.querySelector<HTMLInputElement>('#lastName_create_input');
-const email_create_input = document.querySelector<HTMLInputElement>('#email_create_input');
-const avatar_create_input = document.querySelector<HTMLInputElement>('#avatar_create_input');
+// const firstName_create_input:HTMLInputElement|null = document.querySelector<HTMLInputElement>('#firstName_create_input');
+// const lastName_create_input:HTMLInputElement|null = document.querySelector<HTMLInputElement>('#lastName_create_input');
+// const email_create_input:HTMLInputElement|null = document.querySelector<HTMLInputElement>('#email_create_input');
+// const avatar_create_input:HTMLInputElement|null = document.querySelector<HTMLInputElement>('#avatar_create_input');
+const firstName_create_input = document.getElementById("firstName_create_input") as HTMLInputElement;
+const lastName_create_input = document.getElementById("lastName_create_input") as HTMLInputElement;
+const email_create_input = document.getElementById("email_create_input") as HTMLInputElement;
+const avatar_create_input = document.getElementById("avatar_create_input") as HTMLInputElement;
 
 //update Dom
-const firstName_update_input = document.querySelector<HTMLInputElement>('#firstName_update_input');
-const lastName_update_input = document.querySelector<HTMLInputElement>('#lastName_update_input');
-const email_update_input = document.querySelector<HTMLInputElement>('#email_update_input');
-const avatar_update_input = document.querySelector<HTMLInputElement>('#avatar_update_input');
-const id_update_input = document.querySelector<HTMLInputElement>('#id_update_input');
+// const firstName_update_input:HTMLInputElement|null = document.querySelector<HTMLInputElement>('#firstName_update_input');
+// const lastName_update_input:HTMLInputElement|null = document.querySelector<HTMLInputElement>('#lastName_update_input');
+// const email_update_input:HTMLInputElement|null = document.querySelector<HTMLInputElement>('#email_update_input');
+// const avatar_update_input:HTMLInputElement|null = document.querySelector<HTMLInputElement>('#avatar_update_input');
+// const id_update_input:HTMLInputElement|null = document.querySelector<HTMLInputElement>('#id_update_input');
+const firstName_update_input = document.getElementById("firstName_update_input") as HTMLInputElement;
+const lastName_update_input = document.getElementById("lastName_update_input") as HTMLInputElement;
+const email_update_input = document.getElementById("email_update_input") as HTMLInputElement;
+const avatar_update_input = document.getElementById("avatar_update_input") as HTMLInputElement;
+const id_update_input = document.getElementById("id_update_input") as HTMLInputElement;
 
+//buttons dom
 const create_btn = document.getElementById("create_btn");
 const update_btn = document.getElementById("update_btn");
 const delete_btn = document.getElementById("delete_btn");
+const get_all_users_btn = document.getElementById("get_all_users_btn");
+const get_user_btn = document.getElementById("get_user_btn");
+
 //delete Dom
-const id_delete_input = document.querySelector<HTMLInputElement>('#id_delete_input');
+const id_delete_input = document.getElementById("id_delete_input") as HTMLInputElement;
+//text_area dom
+const text_area = document.getElementById("text_area") as HTMLTextAreaElement;
+//get single user Dom
+const targetUser_id_input = document.getElementById("targetUser_id_input") as HTMLInputElement;
+const targetUser_firstName_input = document.getElementById("targetUser_firstName_input") as HTMLInputElement;
+const targetUser_lastName_input = document.getElementById("targetUser_lastName_input") as HTMLInputElement;
+const targetUser_email_input = document.getElementById("targetUser_email_input") as HTMLInputElement;
+const targetUser_avatar_input = document.getElementById("targetUser_avatar_input") as HTMLInputElement;
 
 //seed localstorage by reqres users
 const seed_btn = document.getElementById("seed")?.addEventListener("click",async(e)=>{
@@ -92,41 +113,62 @@ const avatar = String(avatar_create_input?.value);
 const id = Number(Date.now());
 const newUser:User = {first_name,last_name,email,avatar,id};
 userService.createUser(newUser);
-
-if (firstName_create_input) {
+alert("created successfully")
   firstName_create_input.value = '';
-}
-if (lastName_create_input) {
   lastName_create_input.value = '';
-}
-if (email_create_input) {
   email_create_input.value = '';
-}
-if(avatar_create_input){
-    avatar_create_input.value='';
-}
-
+  avatar_create_input.value='';
 })
 
 
 //update Button function
 update_btn?.addEventListener("click",(e)=>{
 
-const first_name = String(firstName_update_input?.value);
-const last_name = String(lastName_update_input?.value);
-const email = String(email_update_input?.value);
-const avatar = String(avatar_update_input?.value);
-const id = Number(id_update_input?.value)
+const first_name = String(firstName_update_input.value);
+const last_name = String(lastName_update_input.value);
+const email = String(email_update_input.value);
+const avatar = String(avatar_update_input.value);
+const id = Number(id_update_input.value)
 const updateBody: User = {id,first_name,last_name,email,avatar};
 userService.updateUser(updateBody); 
+alert("updated successfully");
+firstName_update_input.value="";
+lastName_update_input.value="";
+id_update_input.value="";
+email_update_input.value="";
+avatar_update_input.value="";
 })
 
 
 //delete Button function
 delete_btn?.addEventListener("click",(e)=>{
-  const id = Number(id_delete_input?.value);
+  const id = Number(id_delete_input.value);
   userService.deleteUser(id);
   if(!!id_delete_input){
     id_delete_input.value="";
   }
+  alert("deleted successfully");
+  id_delete_input.value="";
+})
+
+//get all users and print in index.html
+get_all_users_btn?.addEventListener("click",(e)=>{
+  const usersList = userService.getAllUsers();
+  text_area.value = JSON.stringify(usersList)
+  
+})
+
+//get single user
+get_user_btn?.addEventListener("click",(e)=>{
+  const id= targetUser_id_input.value;
+  const targetUser= userService.readUser(Number(id));
+  if(targetUser){
+    targetUser_firstName_input.value= targetUser.first_name;
+    targetUser_lastName_input.value= targetUser.last_name;
+    targetUser_email_input.value= targetUser.email;
+    targetUser_avatar_input.value= targetUser.avatar;
+
+    
+  }
+  
 })
